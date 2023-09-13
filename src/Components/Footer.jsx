b8beb266b7663b6791be7bad123298039b7c74aa
Pablo Clemente <martin.clemente@cencosud.com>
2023-09-12 21:34:23 -0300

    Entrega final Front3

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../assets/paio-logo.png";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Powered by</p>
+
+      <img src={logo} alt="DH-logo" />
+    </footer>
+  );
+};
+
+export default Footer;
